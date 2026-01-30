8c9a66a16fd945198dcd0507a94c21e4b3eae4e6
Ömer Dumlupınar <omerdumlupinar0@gmail.com>
2026-01-31 02:10:00 +0300

    Catalog Microservice --> ConnectionString (Via Option Pattern)

---
+++ UdemyNewMicroservice.Catalog.Api/Options/OptionExt.cs
@@ -0,0 +1,12 @@
+﻿namespace UdemyNewMicroservice.Catalog.Api.Options
+{
+    public static class OptionExt
+    {
+        public static IServiceCollection AddOptionsExt(this IServiceCollection services)
+        {
+            services.AddOptions<MongoOption>().BindConfiguration(nameof(MongoOption)).ValidateDataAnnotations();
+
+            return services;
+        }
+    }
+}
