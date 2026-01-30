ee530ec38a19565b2d0d74fe264f0339fea26b39
Ömer Dumlupınar <omerdumlupinar0@gmail.com>
2026-01-31 01:30:53 +0300

    Catalog Microservice -->  AppDbContext

---
+++ UdemyNewMicroservice.Catalog.Api/Repositories/CategoryEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MongoDB.EntityFrameworkCore.Extensions;
+using System.Reflection.Emit;
+using UdemyNewMicroservice.Catalog.Api.Features.Categories;
+using UdemyNewMicroservice.Catalog.Api.Features.Courses;
+
+namespace UdemyNewMicroservice.Catalog.Api.Repositories
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToCollection("category");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Ignore(x => x.Courses);
+        }
+    }
+}
---
+++ UdemyNewMicroservice.Catalog.Api/Repositories/CourseEntityConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MongoDB.EntityFrameworkCore.Extensions;
+using System.Reflection.Emit;
+using UdemyNewMicroservice.Catalog.Api.Features.Courses;
+
+namespace UdemyNewMicroservice.Catalog.Api.Repositories
+{
+    public class CourseEntityConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.ToCollection("courses");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Name).HasElementName("name").HasMaxLength(100);
+            builder.Property(x => x.Description).HasElementName("description").HasMaxLength(1000);
+            builder.Property(x => x.Picture).HasElementName("picture");
+            builder.Property(x => x.Price).HasElementName("price");
+            builder.Property(x => x.CreatedDate).HasElementName("createdate");
+            builder.Property(x => x.CategoruId).HasElementName("categoryid");
+            builder.Property(x => x.UserId).HasElementName("userid");
+            builder.Ignore(x => x.Category);
+
+            builder.OwnsOne(c => c.Feature, feature =>
+            {
+                feature.HasElementName("feature");
+                feature.Property(x => x.Duration).HasElementName("duration");
+                feature.Property(x => x.Rating).HasElementName("rating");
+                feature.Property(x => x.EducationFullName).HasElementName("educationfullname");
+            });
+        }
+    }
+}
